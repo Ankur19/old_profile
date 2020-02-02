804f559445ef477f10f92ac79a6df3849506c35d
anksaiki <asaikia@hawk.iit.edu>
2020-02-02 03:01:07 -0600

    Updates

---
+++ precache-manifest.1c7fef60c80cf83201a07bd0afbe43d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44614b67d1a77ed3ef7b88fc4d52ed94",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "bc62b314d206f8e09b17",
+    "url": "/profile/static/css/main.1096feaf.chunk.css"
+  },
+  {
+    "revision": "835514ee1ac36b481bbd",
+    "url": "/profile/static/js/2.a0df8db3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.a0df8db3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc62b314d206f8e09b17",
+    "url": "/profile/static/js/main.d79ab1c0.chunk.js"
+  },
+  {
+    "revision": "b5a7d259590e62b51bcc",
+    "url": "/profile/static/js/runtime-main.3026efc4.js"
+  },
+  {
+    "revision": "44f1ad020222b4b2f6a60db34cfc8df7",
+    "url": "/profile/static/media/1165.44f1ad02.jpg"
+  },
+  {
+    "revision": "a9001e835b8795bacffd86a1e06424ec",
+    "url": "/profile/static/media/2401.a9001e83.jpg"
+  },
+  {
+    "revision": "6dbcbf028ba7e572adb77b557700e1a8",
+    "url": "/profile/static/media/3089.6dbcbf02.jpg"
+  },
+  {
+    "revision": "dbee5b6027183a27ffe4b212b8ad022d",
+    "url": "/profile/static/media/3102.dbee5b60.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a58784aa4410d1e96434a3cabb960e42.js"
+  "/profile/precache-manifest.1c7fef60c80cf83201a07bd0afbe43d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
